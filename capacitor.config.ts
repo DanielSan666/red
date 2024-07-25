a97ac221bd574d96705f9673969b125fd74993de
ed03e2 <ed03e2@gmail.com>
2024-07-25 22:45:53 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RedSocial',
+  webDir: 'www'
+};
+
+export default config;
