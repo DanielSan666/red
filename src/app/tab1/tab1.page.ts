e1d7dab9da21c57010179c9845fa408f2c467b2e
Junior Castañeda Rios <bc4363624@gmail.com>
2024-08-09 18:58:16 -0600

    v5

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
 import { SocialService } from '../services/social/social.service';
-import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from '../services/perfil/perfil.service';
 import { ModalComponentAddComponent } from '../modal-component-add/modal-component-add.component';
 import { ModalComponentComponent } from '../modal-component/modal-component.component';
-import { ActionSheetController, NavController,ModalController } from '@ionic/angular';
+import {
+  ActionSheetController,
+  NavController,
+  ModalController,
+} from '@ionic/angular';
 import { LoadingController, ToastController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { DataService } from '../services/publicacion/data.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
   comments: any[] = [];
@@ -22,7 +25,7 @@ export class Tab1Page {
   posts: any;
   currentUser: any;
   newComment: string = '';
-  users: { [key: string]: any } = {}; 
+  users: { [key: string]: any } = {};
   public actionSheetButtons = [
     {
       text: 'Eliminar',
@@ -56,74 +59,72 @@ export class Tab1Page {
     private userService: UserService,
     private afAuth: AngularFireAuth,
     private social: SocialService
-
   ) {
-    
     addIcons({ add });
   }
   share() {
     if (navigator.share) {
-      navigator.share({
-        title: 'Red-Social',
-        text: 'Mira lo que comparti en esta publicacion',
-        url: 'https://Red-Social.com'
-      })
-      .then(() => {
-        console.log('Shared successfully!');
-      })
-      .catch((error) => {
-        console.error('Error sharing:', error);
-      });
+      navigator
+        .share({
+          title: 'Red-Social',
+          text: 'Mira lo que comparti en esta publicacion',
+          url: 'https://Red-Social.com',
+        })
+        .then(() => {
+          console.log('Shared successfully!');
+        })
+        .catch((error) => {
+          console.error('Error sharing:', error);
+        });
     } else {
       console.log('Sharing not supported');
     }
   }
 
-
   ionViewWillEnter() {
     this.getPosts();
   }
   async ngOnInit() {
-    this.afAuth.authState.subscribe(user => {
+    this.afAuth.authState.subscribe((user) => {
       this.currentUser = user;
     });
-      // Escuchar cambios en tiempo real para comentarios
-      this.social.getComments(this.posts).subscribe(comments => {
-        this.comments = comments;
-      });
-  
-      // Escuchar cambios en tiempo real para reacciones
-      this.social.getReactions(this.posts).subscribe(reactions => {
-        this.reactions = reactions;
-      });
+    // Escuchar cambios en tiempo real para comentarios
+    this.social.getComments(this.posts).subscribe((comments) => {
+      this.comments = comments;
+    });
+
+    // Escuchar cambios en tiempo real para reacciones
+    this.social.getReactions(this.posts).subscribe((reactions) => {
+      this.reactions = reactions;
+    });
   }
   addComment(comment: string) {
-    const userId = this.currentUser;  // Reemplaza con el ID real del usuario
+    const userId = this.currentUser; // Reemplaza con el ID real del usuario
     this.social.addComment(this.posts, comment, userId);
   }
 
   addReaction() {
-    const userId = this.currentUser;  // Reemplaza con el ID real del usuario
+    const userId = this.currentUser; // Reemplaza con el ID real del usuario
     this.social.addReaction(this.posts, userId);
   }
 
   loadUsersForPosts(posts: any[]) {
-    const userIds = [...new Set(posts.map(post => post.userId))]; // Obtener IDs únicos
-    userIds.forEach(userId => {
-      this.dataService.getUserById(userId).subscribe(user => {
+    const userIds = [...new Set(posts.map((post) => post.userId))]; // Obtener IDs únicos
+    userIds.forEach((userId) => {
+      this.dataService.getUserById(userId).subscribe((user) => {
         this.users[userId] = user;
       });
     });
   }
 
-
   async getPosts() {
     let loader = await this.loadingCtrl.create({
-      message: "Espere por favor..."
+      message: 'Espere por favor...',
     });
     await loader.present();
     try {
-      this.afStore.collection('posts')
+      this.afStore
+        .collection('posts')
         .snapshotChanges()
         .subscribe((data: any[]) => {
           this.posts = data.map((e: any) => {
@@ -134,7 +135,7 @@ export class Tab1Page {
               contenido: postData.contenido,
               detalles: postData.detalles,
               imageUrl: postData.imageUrl,
-              userId: postData.userId
+              userId: postData.userId,
             };
           });
           this.loadUsersForPosts(this.posts);
@@ -149,11 +150,11 @@ export class Tab1Page {
   }
   async deletePost(id: string) {
     let loader = await this.loadingCtrl.create({
-      message: "Espere por favor..."
+      message: 'Espere por favor...',
     });
     await loader.present();
     try {
-      await this.afStore.doc("posts/" + id).delete();
+      await this.afStore.doc('posts/' + id).delete();
       await loader.dismiss();
     } catch (e: any) {
       await loader.dismiss();
@@ -163,21 +164,23 @@ export class Tab1Page {
   }
 
   showToast(message: string) {
-    this.toastCtrl.create({
-      message: message,
-      duration: 5000
-    }).then(toastData => toastData.present());
+    this.toastCtrl
+      .create({
+        message: message,
+        duration: 5000,
+      })
+      .then((toastData) => toastData.present());
   }
 
   async presentActionSheet(id: string) {
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Acciones',
-      buttons: this.actionSheetButtons.map(button => ({
+      buttons: this.actionSheetButtons.map((button) => ({
         ...button,
         handler: () => {
           this.handleAction(button.data.action, id);
-        }
-      }))
+        },
+      })),
     });
     await actionSheet.present();
   }
@@ -205,8 +208,8 @@ export class Tab1Page {
     const modal = await this.modalCtrl.create({
       component: ModalComponentComponent,
       componentProps: {
-        id: postId // Pasar el ID como un prop
-      }
+        id: postId, // Pasar el ID como un prop
+      },
     });
     return await modal.present();
   }
@@ -214,15 +217,12 @@ export class Tab1Page {
   async openModal() {
     const modal = await this.modalCtrl.create({
       component: ModalComponentAddComponent,
-      componentProps: {
-      }
+      componentProps: {},
     });
     return await modal.present();
 
-    const {data,  role } = await modal.onWillDismiss();
+    const { data, role } = await modal.onWillDismiss();
     if (role === 'confirm') {
     }
   }
-
-
 }
